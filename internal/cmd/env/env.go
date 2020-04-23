b299c0aaefebc4e15a2b815ad3a2b1ba0495785a
Elizabeth Harper <epharper@upenn.edu>
2020-04-23 14:12:26 -0400

    added JAVA_HOME passthrough due to env config on our sites

--- internal/cmd/env/env.go
+++ internal/cmd/env/env.go
@@ -1,13 +1,22 @@
 package env
 
-import "github.com/VEuPathDB/script-api-test-runner/internal/conf"
+import (
+	"github.com/VEuPathDB/script-api-test-runner/internal/conf"
+	"os"
+)
 
 func BuildEnv(opts *conf.Options) []string {
-	return []string {
+	out := []string {
 		BuildUsersEnv(opts),
 		BuildAuthEnv(opts),
 		BuildSiteUrlEnv(opts),
 		BuildTokenEnv(opts),
 		BuildVerboseEnv(opts),
 	}
+
+	if val, ok := os.LookupEnv("JAVA_HOME"); ok {
+		out = append(out, "JAVA_HOME=" + val)
+	}
+
+	return out
 }
